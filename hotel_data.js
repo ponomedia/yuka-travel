e2d9d025276a89c9e5d3f4683ce9d31b8ca1e1be
ponomedia <hayato0626hawaks@gmail.com>
2026-04-09 15:06:13 +0900

    research: 登別温泉　第一滝本館 (登別) 2026-04-09

--- hotel_data.js
+++ hotel_data.js
@@ -1,11 +1,13 @@
-// このファイルはtravel_hotel_poster.pyが投稿のたびに自動更新します
+// このファイルはtravel_hotel_researcher.pyが自動更新します
 const HOTEL_DATA = {
-  "name": "奥日光　ホテル四季彩",
-  "area": "日光・鬼怒川",
-  "address": "栃木県日光市中宮祠2485",
-  "review": "4.31",
-  "review_count": 2098,
-  "min_charge": 9900,
-  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/13462/13462.jpg",
-  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D13462%26f_flg%3DPLAN"
+  "name": "登別温泉　第一滝本館",
+  "area": "登別",
+  "review": "4.56",
+  "review_count": 3348,
+  "min_charge": 11000,
+  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D30109%26f_flg%3DPLAN",
+  "local_images": [
+    "travel_landing\\hotel_images\\登別温泉_第一滝本館_1.jpg"
+  ],
+  "researched_at": "2026-04-09"
 };
