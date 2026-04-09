c26e57ec2bc3c1d284a0aa0e79a31cea65a1fcac
ponomedia <hayato0626hawaks@gmail.com>
2026-04-09 13:02:48 +0900

    update: Ｒａｋｕｔｅｎ　ＳＴＡＹ　ＶＩＬＬＡ　日光 (日光・鬼怒川)

--- hotel_data.js
+++ hotel_data.js
@@ -1,10 +1,11 @@
+// このファイルはtravel_hotel_poster.pyが投稿のたびに自動更新します
 const HOTEL_DATA = {
-  "name": "登別温泉　第一滝本館",
-  "area": "登別",
-  "address": "北海道登別市登別温泉町55",
-  "review": "4.56",
-  "review_count": 3348,
-  "min_charge": 11000,
-  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/30109/30109.jpg",
-  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/sample/"
+  "name": "Ｒａｋｕｔｅｎ　ＳＴＡＹ　ＶＩＬＬＡ　日光",
+  "area": "日光・鬼怒川",
+  "address": "栃木県日光市所野1550-6　Ｒａｋｕｔｅｎ　ＳＴＡＹ　ＶＩＬＬＡ　日光",
+  "review": "4.51",
+  "review_count": 216,
+  "min_charge": 7560,
+  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/186671/186671.jpg",
+  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D186671%26f_flg%3DPLAN"
 };
