8e7e64fafdeb6d67a20cf3b4b3d2c6164eb3e02a
ponomedia <hayato0626hawaks@gmail.com>
2026-04-09 15:01:37 +0900

    update: 奥日光　ホテル四季彩 (日光・鬼怒川)

--- hotel_data.js
+++ hotel_data.js
@@ -1,11 +1,11 @@
 // このファイルはtravel_hotel_poster.pyが投稿のたびに自動更新します
 const HOTEL_DATA = {
-  "name": "Ｒａｋｕｔｅｎ　ＳＴＡＹ　ＶＩＬＬＡ　日光",
+  "name": "奥日光　ホテル四季彩",
   "area": "日光・鬼怒川",
-  "address": "栃木県日光市所野1550-6　Ｒａｋｕｔｅｎ　ＳＴＡＹ　ＶＩＬＬＡ　日光",
-  "review": "4.51",
-  "review_count": 216,
-  "min_charge": 7560,
-  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/186671/186671.jpg",
-  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D186671%26f_flg%3DPLAN"
+  "address": "栃木県日光市中宮祠2485",
+  "review": "4.31",
+  "review_count": 2098,
+  "min_charge": 9900,
+  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/13462/13462.jpg",
+  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D13462%26f_flg%3DPLAN"
 };
