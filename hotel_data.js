a600465d0ab3c6e77ce9657dd25bd8dd0334131c
ponomedia <hayato0626hawaks@gmail.com>
2026-04-10 07:55:30 +0900

    fix: アフィリエイトURLを画像APIから正しい予約ページURLに修正

--- hotel_data.js
+++ hotel_data.js
@@ -8,7 +8,7 @@ const HOTEL_LIST = [
     "review_count": 3516,
     "min_charge": 12100,
     "image": "https://img.travel.rakuten.co.jp/share/HOTEL/68224/68224.jpg",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D68224%26f_flg%3DPLAN"
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F68224%2F"
   },
   {
     "name": "ＨＯＴＥＬシーサイド島原",
@@ -18,7 +18,7 @@ const HOTEL_LIST = [
     "review_count": 430,
     "min_charge": 6600,
     "image": "https://img.travel.rakuten.co.jp/share/HOTEL/139921/139921_kya.jpg",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D139921%26f_flg%3DPLAN"
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F139921%2F"
   },
   {
     "name": "雲仙温泉　ゆやど　雲仙新湯",
@@ -28,7 +28,7 @@ const HOTEL_LIST = [
     "review_count": 648,
     "min_charge": 8662,
     "image": "https://img.travel.rakuten.co.jp/share/HOTEL/31749/31749.jpg",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D31749%26f_flg%3DPLAN"
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F31749%2F"
   },
   {
     "name": "雲仙温泉　民芸モダンの宿　雲仙福田屋",
@@ -38,7 +38,7 @@ const HOTEL_LIST = [
     "review_count": 802,
     "min_charge": 12100,
     "image": "https://img.travel.rakuten.co.jp/share/HOTEL/6194/6194.jpg",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D6194%26f_flg%3DPLAN"
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F6194%2F"
   },
   {
     "name": "雲仙温泉・源泉かけ流し＆おしどりの池を望む美食の宿　東園",
@@ -48,6 +48,6 @@ const HOTEL_LIST = [
     "review_count": 467,
     "min_charge": 11385,
     "image": "https://img.travel.rakuten.co.jp/share/HOTEL/41803/41803.jpg",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D41803%26f_flg%3DPLAN"
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F41803%2F"
   }
 ];
