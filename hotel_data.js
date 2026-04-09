f4d888764276556e637ab5d0c0351f3980007197
ponomedia <hayato0626hawaks@gmail.com>
2026-04-09 16:14:52 +0900

    update: 雲仙温泉・源泉かけ流し＆おしどりの池を望む美食の宿　東園 (雲仙・島原)

--- hotel_data.js
+++ hotel_data.js
@@ -1,11 +1,11 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_DATA = {
-  "name": "雲仙九州ホテル",
+  "name": "雲仙温泉・源泉かけ流し＆おしどりの池を望む美食の宿　東園",
   "area": "雲仙・島原",
-  "address": "長崎県雲仙市小浜町雲仙320",
-  "review": "4.64",
-  "review_count": 682,
-  "min_charge": 23100,
-  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/4915/4915.jpg",
-  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D4915%26f_flg%3DPLAN"
+  "address": "長崎県雲仙市小浜町雲仙181",
+  "review": "4.54",
+  "review_count": 467,
+  "min_charge": 11385,
+  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/41803/41803.jpg",
+  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D41803%26f_flg%3DPLAN"
 };
