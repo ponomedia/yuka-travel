7b6a2ea48742f06178f20a44f1ad3d0621a62711
ponomedia <hayato0626hawaks@gmail.com>
2026-04-09 13:01:31 +0900

    Initial commit: ゆか旅行ランディングページ

---
+++ hotel_data.js
@@ -0,0 +1,10 @@
+const HOTEL_DATA = {
+  "name": "登別温泉　第一滝本館",
+  "area": "登別",
+  "address": "北海道登別市登別温泉町55",
+  "review": "4.56",
+  "review_count": 3348,
+  "min_charge": 11000,
+  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/30109/30109.jpg",
+  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/sample/"
+};
