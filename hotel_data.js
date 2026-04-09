471f4fd17d665c6ba384c66d50468b55fe5aab5b
ponomedia <hayato0626hawaks@gmail.com>
2026-04-09 16:21:14 +0900

    add: 雲仙温泉　民芸モダンの宿　雲仙福田屋 (雲仙・島原)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "雲仙温泉　民芸モダンの宿　雲仙福田屋",
+    "area": "雲仙・島原",
+    "address": "長崎県雲仙市小浜町雲仙380-2",
+    "review": "4.6",
+    "review_count": 802,
+    "min_charge": 12100,
+    "image": "https://img.travel.rakuten.co.jp/share/HOTEL/6194/6194.jpg",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D6194%26f_flg%3DPLAN"
+  },
+  {
     "name": "雲仙温泉・源泉かけ流し＆おしどりの池を望む美食の宿　東園",
     "area": "雲仙・島原",
     "address": "長崎県雲仙市小浜町雲仙181",
