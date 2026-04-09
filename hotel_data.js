cb46559f717ac0b2f777b554312e500ba5553c87
ponomedia <hayato0626hawaks@gmail.com>
2026-04-09 15:47:11 +0900

    update: 雲仙九州ホテル (雲仙・島原)

--- hotel_data.js
+++ hotel_data.js
@@ -1,13 +1,11 @@
-// このファイルはtravel_hotel_researcher.pyが自動更新します
+// このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_DATA = {
-  "name": "登別温泉　第一滝本館",
-  "area": "登別",
-  "review": "4.56",
-  "review_count": 3348,
-  "min_charge": 11000,
-  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D30109%26f_flg%3DPLAN",
-  "local_images": [
-    "travel_landing\\hotel_images\\登別温泉_第一滝本館_1.jpg"
-  ],
-  "researched_at": "2026-04-09"
+  "name": "雲仙九州ホテル",
+  "area": "雲仙・島原",
+  "address": "長崎県雲仙市小浜町雲仙320",
+  "review": "4.64",
+  "review_count": 682,
+  "min_charge": 23100,
+  "image": "https://img.travel.rakuten.co.jp/share/HOTEL/4915/4915.jpg",
+  "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Fimg.travel.rakuten.co.jp%2Fimage%2Ftr%2Fapi%2Fhs%2FcHNRi%2F%3Ff_no%3D4915%26f_flg%3DPLAN"
 };
