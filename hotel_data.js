9e30e2b73f47b2a584e18ec7459d7135df6653b0
ponomedia <hayato0626hawaks@gmail.com>
2026-04-13 09:01:50 +0900

    add: 伊香保温泉　千明仁泉亭（ちぎらじんせんてい） (伊香保温泉)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "伊香保温泉　千明仁泉亭（ちぎらじんせんてい）",
+    "area": "伊香保温泉",
+    "address": "群馬県渋川市伊香保町伊香保　４５番地",
+    "review": "4.54",
+    "review_count": 456,
+    "min_charge": 15840,
+    "image": "https://trvimg.r10s.jp/share/image_up/147464/origin/334564a77e5191c5c5a307432e6ac9f0a1f6e1d9.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F147464%2F"
+  },
+  {
     "name": "伊香保温泉　心に咲く花　古久家（こくや）",
     "area": "伊香保温泉",
     "address": "群馬県渋川市伊香保町伊香保52",
