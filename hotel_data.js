4f54e49c853920f28f7a2f92c01b39e5baf28d4f
ponomedia <hayato0626hawaks@gmail.com>
2026-04-14 17:01:48 +0900

    add: ＨＩＹＯＲＩオーシャンリゾート沖縄 (恩納・名護)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "ＨＩＹＯＲＩオーシャンリゾート沖縄",
+    "area": "恩納・名護",
+    "address": "沖縄県国頭郡恩納村仲泊1055-1",
+    "review": "4.69",
+    "review_count": 372,
+    "min_charge": 11988,
+    "image": "https://trvimg.r10s.jp/share/image_up/180687/origin/4a000c015da5813ccb8a3b16ea31bc6d32c206c4.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F180687%2F"
+  },
+  {
     "name": "ホテルモントレ沖縄　スパ＆リゾート",
     "area": "恩納・名護",
     "address": "沖縄県国頭郡恩納村字冨着1550-1",
