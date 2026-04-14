d8bcdd956b80e3b55765f94927559a8a497af316
ponomedia <hayato0626hawaks@gmail.com>
2026-04-14 09:01:48 +0900

    add: カヌチャベイホテル＆ヴィラズ (恩納・名護)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "カヌチャベイホテル＆ヴィラズ",
+    "area": "恩納・名護",
+    "address": "沖縄県名護市安部156-2",
+    "review": "4.37",
+    "review_count": 1310,
+    "min_charge": 9000,
+    "image": "https://trvimg.r10s.jp/share/image_up/29766/origin/558503043c1783d2118a15cc8b56368278e55888.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F29766%2F"
+  },
+  {
     "name": "ルネッサンスリゾートオキナワ",
     "area": "恩納・名護",
     "address": "沖縄県国頭郡恩納村山田3425-2",
