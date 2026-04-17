b3730f657c877fea0e9084d1d6ee56f5cf9420ae
ponomedia <hayato0626hawaks@gmail.com>
2026-04-17 14:01:42 +0900

    add: ラビスタ函館ベイ（共立リゾート） (函館)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "ラビスタ函館ベイ（共立リゾート）",
+    "area": "函館",
+    "address": "北海道函館市豊川町12-6",
+    "review": "4.45",
+    "review_count": 5277,
+    "min_charge": 11063,
+    "image": "https://trvimg.r10s.jp/share/image_up/69295/origin/fd0223bfd3d07c4093559f1c038650426c0bc35c.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F69295%2F"
+  },
+  {
     "name": "函館湯の川温泉　海と灯／ヒューイットリゾート",
     "area": "函館",
     "address": "北海道函館市湯川町3-9-20",
