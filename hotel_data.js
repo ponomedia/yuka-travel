bc8a2a5f8ca11a7ed60524979fd8a51a46c5a2ca
ponomedia <hayato0626hawaks@gmail.com>
2026-05-28 18:02:59 +0900

    add: 強羅温泉　メルヴェール箱根強羅 (箱根)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "強羅温泉　メルヴェール箱根強羅",
+    "area": "箱根",
+    "address": "神奈川県足柄下郡箱根町強羅1300-70",
+    "review": "4.04",
+    "review_count": 4480,
+    "min_charge": 8800,
+    "image": "https://trvimg.r10s.jp/share/image_up/5066/origin/a1bdb9ec9d195ff1c3559d47d9bb1c74ee26dcac.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F5066%2F"
+  },
+  {
     "name": "箱根湯本温泉　ホテル　おかだ",
     "area": "箱根",
     "address": "神奈川県足柄下郡箱根町湯本茶屋191",
