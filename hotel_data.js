3e68af5a087c5a7ddc0587ecbe5a07ff57d8fed0
ponomedia <hayato0626hawaks@gmail.com>
2026-05-29 21:03:08 +0900

    add: 熱海温泉　熱海ニューフジヤホテル（伊東園ホテルズ） (熱海)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "熱海温泉　熱海ニューフジヤホテル（伊東園ホテルズ）",
+    "area": "熱海",
+    "address": "静岡県熱海市銀座町1-16",
+    "review": "3.7",
+    "review_count": 3358,
+    "min_charge": 7986,
+    "image": "https://trvimg.r10s.jp/share/image_up/1459/origin/4ee23425471eb4779c4f743b0fc5dc0fb7e266a1.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F1459%2F"
+  },
+  {
     "name": "リブマックスリゾート熱海Ｏｃｅａｎ",
     "area": "熱海",
     "address": "静岡県熱海市渚町22-8",
