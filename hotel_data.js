ad001db9d6de5c45776c18345091939f892779d4
ponomedia <hayato0626hawaks@gmail.com>
2026-06-01 11:02:29 +0900

    add: 伊香保温泉　ホテル木暮 (伊香保温泉)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "伊香保温泉　ホテル木暮",
+    "area": "伊香保温泉",
+    "address": "群馬県渋川市伊香保町伊香保135",
+    "review": "4.72",
+    "review_count": 1915,
+    "min_charge": 19800,
+    "image": "https://trvimg.r10s.jp/share/image_up/6266/origin/185de8658f00caa84f394553e27713ef263ef5b4.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F6266%2F"
+  },
+  {
     "name": "伊香保温泉　ホテル松本楼",
     "area": "伊香保温泉",
     "address": "群馬県渋川市伊香保町伊香保164",
@@ -649,16 +659,6 @@ const HOTEL_LIST = [
     "min_charge": 9350,
     "image": "https://trvimg.r10s.jp/share/image_up/28606/origin/dd26df9ae2657628aa490bc0b11250b4d2fc067d.47.9.26.3.jpg?fit=inside%7C888:498",
     "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F28606%2F"
-  },
-  {
-    "name": "伊香保温泉　ホテル木暮",
-    "area": "伊香保温泉",
-    "address": "群馬県渋川市伊香保町伊香保135",
-    "review": "4.74",
-    "review_count": 1867,
-    "min_charge": 19800,
-    "image": "https://trvimg.r10s.jp/share/image_up/6266/origin/185de8658f00caa84f394553e27713ef263ef5b4.47.9.26.3.jpg?fit=inside%7C888:498",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F6266%2F"
   },
   {
     "name": "杜の湯　きらの里（共立リゾート）（２０２５年１２月２２日リニューアルオープン）",
