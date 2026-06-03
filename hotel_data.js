d16cd963b52f511def01592f0fc5e70671a5b591
ponomedia <hayato0626hawaks@gmail.com>
2026-06-03 11:03:24 +0900

    add: ルネッサンスリゾートオキナワ (恩納・名護)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "ルネッサンスリゾートオキナワ",
+    "area": "恩納・名護",
+    "address": "沖縄県国頭郡恩納村山田3425-2",
+    "review": "4.63",
+    "review_count": 860,
+    "min_charge": 18400,
+    "image": "https://trvimg.r10s.jp/share/image_up/54315/origin/75a61f1a830ecb1e6542125423bdfc4554e8b85c.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F54315%2F"
+  },
+  {
     "name": "ハレクラニ沖縄",
     "area": "恩納・名護",
     "address": "沖縄県国頭郡恩納村名嘉真1967-1",
@@ -649,16 +659,6 @@ const HOTEL_LIST = [
     "min_charge": 5390,
     "image": "https://trvimg.r10s.jp/share/image_up/52229/origin/4d89cd65a695aa9948526b19140518701c7e0f5f.47.9.26.3.jpg?fit=inside%7C888:498",
     "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F52229%2F"
-  },
-  {
-    "name": "ルネッサンスリゾートオキナワ",
-    "area": "恩納・名護",
-    "address": "沖縄県国頭郡恩納村山田3425-2",
-    "review": "4.66",
-    "review_count": 823,
-    "min_charge": 14500,
-    "image": "https://trvimg.r10s.jp/share/image_up/54315/origin/75a61f1a830ecb1e6542125423bdfc4554e8b85c.47.9.26.3.jpg?fit=inside%7C888:498",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F54315%2F"
   },
   {
     "name": "伊香保温泉　森秋旅館",
