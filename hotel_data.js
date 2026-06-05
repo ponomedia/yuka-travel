3b24b0b213d13c17e8df08ae7b918e5def223ef2
ponomedia <hayato0626hawaks@gmail.com>
2026-06-05 21:02:25 +0900

    add: 定山渓　花もみじ (定山渓)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "定山渓　花もみじ",
+    "area": "定山渓",
+    "address": "北海道札幌市南区定山渓温泉西3丁目32番地",
+    "review": "4.38",
+    "review_count": 1155,
+    "min_charge": 17500,
+    "image": "https://trvimg.r10s.jp/share/image_up/54584/origin/044995dad2d779b816615f4535ea2483377e788c.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F54584%2F"
+  },
+  {
     "name": "定山渓温泉　定山渓第一寶亭留　翠山亭",
     "area": "定山渓",
     "address": "北海道札幌市南区定山渓温泉西3-105",
