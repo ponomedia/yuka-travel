d95bb540e60824293a1a52c766a3b1a2a8efa97d
ponomedia <hayato0626hawaks@gmail.com>
2026-06-06 09:03:00 +0900

    add: 定山渓鶴雅リゾートスパ森の謌 (定山渓)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "定山渓鶴雅リゾートスパ森の謌",
+    "area": "定山渓",
+    "address": "北海道札幌市南区定山渓温泉東3-192",
+    "review": "4.5",
+    "review_count": 712,
+    "min_charge": 22112,
+    "image": "https://trvimg.r10s.jp/share/image_up/107757/origin/64c164aa21116d52425ea5d5d806df7be9f5e229.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F107757%2F"
+  },
+  {
     "name": "定山渓　花もみじ",
     "area": "定山渓",
     "address": "北海道札幌市南区定山渓温泉西3丁目32番地",
