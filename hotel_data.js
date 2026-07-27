2aa6c71e7bef5d8df799de25269165e8f14c0bb5
ponomedia <hayato0626hawaks@gmail.com>
2026-07-27 18:02:16 +0900

    add: 伊香保温泉　ホテル木暮 (伊香保温泉)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "伊香保温泉　ホテル木暮",
+    "area": "伊香保温泉",
+    "address": "群馬県渋川市伊香保町伊香保135",
+    "review": "4.74",
+    "review_count": 1953,
+    "min_charge": 19800,
+    "image": "https://trvimg.r10s.jp/share/image_up/6266/origin/185de8658f00caa84f394553e27713ef263ef5b4.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F6266%2F"
+  },
+  {
     "name": "渋川天然温泉　ハナホテル＆スパ　伊香保インター",
     "area": "伊香保温泉",
     "address": "群馬県渋川市半田3129-1",
@@ -459,16 +469,6 @@ const HOTEL_LIST = [
     "min_charge": 9240,
     "image": "https://trvimg.r10s.jp/share/image_up/28606/origin/dd26df9ae2657628aa490bc0b11250b4d2fc067d.47.9.26.3.jpg?fit=inside%7C888:498",
     "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F28606%2F"
-  },
-  {
-    "name": "伊香保温泉　ホテル木暮",
-    "area": "伊香保温泉",
-    "address": "群馬県渋川市伊香保町伊香保135",
-    "review": "4.72",
-    "review_count": 1915,
-    "min_charge": 19800,
-    "image": "https://trvimg.r10s.jp/share/image_up/6266/origin/185de8658f00caa84f394553e27713ef263ef5b4.47.9.26.3.jpg?fit=inside%7C888:498",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F6266%2F"
   },
   {
     "name": "箱根湯本温泉　ホテル　おかだ",
