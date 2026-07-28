bc2bed22ef46c070044cd8335a87b14757d73309
ponomedia <hayato0626hawaks@gmail.com>
2026-07-28 18:02:32 +0900

    add: 草津温泉　ホテルヴィレッジ (草津温泉)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "草津温泉　ホテルヴィレッジ",
+    "area": "草津温泉",
+    "address": "群馬県吾妻郡草津町大字草津618番地",
+    "review": "4.36",
+    "review_count": 4788,
+    "min_charge": 13750,
+    "image": "https://trvimg.r10s.jp/share/image_up/5270/origin/557fbdef33d0687ddbd78aba79321d05ab775fed.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F5270%2F"
+  },
+  {
     "name": "草津温泉　草津ナウリゾートホテル",
     "area": "草津温泉",
     "address": "群馬県吾妻郡草津町草津白根750",
