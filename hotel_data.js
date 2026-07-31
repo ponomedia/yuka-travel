20fcea4918058a2a250c0cc25df98c3e12abf582
ponomedia <hayato0626hawaks@gmail.com>
2026-07-31 18:02:20 +0900

    add: ＥＮ　ＲＥＳＯＲＴ　Ｒｅ’Ｃｏｖｅ　Ｈａｋｏｎｅ（旧：リ・カーヴ箱根） (箱根)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "ＥＮ　ＲＥＳＯＲＴ　Ｒｅ’Ｃｏｖｅ　Ｈａｋｏｎｅ（旧：リ・カーヴ箱根）",
+    "area": "箱根",
+    "address": "神奈川県足柄下郡箱根町仙石原934-25",
+    "review": "4.27",
+    "review_count": 6978,
+    "min_charge": 11500,
+    "image": "https://trvimg.r10s.jp/share/image_up/15250/origin/bcaa837ac9bf9929f95ec72e309120b330cf80e7.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F15250%2F"
+  },
+  {
     "name": "富士屋ホテル",
     "area": "箱根",
     "address": "神奈川県足柄下郡箱根町宮ノ下359",
