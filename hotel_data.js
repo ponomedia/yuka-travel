7d7150b507eb9bac30175c841dd085510654a636
ponomedia <hayato0626hawaks@gmail.com>
2026-08-02 14:02:43 +0900

    add: 熱海温泉　味と湯の宿　ニューとみよし (熱海)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "熱海温泉　味と湯の宿　ニューとみよし",
+    "area": "熱海",
+    "address": "静岡県熱海市下多賀1472-1",
+    "review": "4.58",
+    "review_count": 2257,
+    "min_charge": 28600,
+    "image": "https://trvimg.r10s.jp/share/image_up/7616/origin/b954904d828e62429c30037dfd3ce1f72adac117.47.9.26.3.jpeg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F7616%2F"
+  },
+  {
     "name": "熱海倶楽部　迎賓館",
     "area": "熱海",
     "address": "静岡県熱海市伊豆山1171",
