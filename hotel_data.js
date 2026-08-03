d254ebffdbf0480e8a7ba5690b8d1c91d4d20466
ponomedia <hayato0626hawaks@gmail.com>
2026-08-03 09:03:34 +0900

    add: 由布院温泉　御宿　ゆふいん亭 (湯布院)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "由布院温泉　御宿　ゆふいん亭",
+    "area": "湯布院",
+    "address": "大分県由布市湯布院町川上山畔1027-3",
+    "review": "4.61",
+    "review_count": 789,
+    "min_charge": 18000,
+    "image": "https://trvimg.r10s.jp/share/image_up/41838/origin/567ccd00b8bb22a9e4d737b2a0f5f3bcce4fe7f1.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F41838%2F"
+  },
+  {
     "name": "熱海温泉　月の栖　熱海聚楽ホテル",
     "area": "熱海",
     "address": "静岡県熱海市田原本町2-19",
@@ -529,16 +539,6 @@ const HOTEL_LIST = [
     "min_charge": 23700,
     "image": "https://trvimg.r10s.jp/share/image_up/106080/origin/fab91ac7cde16ee9b536e3b198383bea93dc0fdf.47.9.26.3.jpg?fit=inside%7C888:498",
     "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F106080%2F"
-  },
-  {
-    "name": "由布院温泉　御宿　ゆふいん亭",
-    "area": "湯布院",
-    "address": "大分県由布市湯布院町川上山畔1027-3",
-    "review": "4.55",
-    "review_count": 760,
-    "min_charge": 17000,
-    "image": "https://trvimg.r10s.jp/share/image_up/41838/origin/567ccd00b8bb22a9e4d737b2a0f5f3bcce4fe7f1.47.9.26.3.jpg?fit=inside%7C888:498",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F41838%2F"
   },
   {
     "name": "由布院温泉　かほりの郷　はな村",
