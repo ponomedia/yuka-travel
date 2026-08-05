cb28a19c6f89526b66971696a7b1e8820febfb47
ponomedia <hayato0626hawaks@gmail.com>
2026-08-05 21:03:02 +0900

    add: オリオンホテル　モトブリゾート＆スパ (恩納・名護)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "オリオンホテル　モトブリゾート＆スパ",
+    "area": "恩納・名護",
+    "address": "沖縄県国頭郡本部町備瀬148-1",
+    "review": "4.56",
+    "review_count": 903,
+    "min_charge": 15900,
+    "image": "https://trvimg.r10s.jp/share/image_up/145419/origin/ad1c95df164174a38ae9107b1ac1554ed7bd0ea0.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F145419%2F"
+  },
+  {
     "name": "リザン　シーパークホテル谷茶ベイ",
     "area": "恩納・名護",
     "address": "沖縄県国頭郡恩納村谷茶1496",
@@ -629,16 +639,6 @@ const HOTEL_LIST = [
     "min_charge": 23100,
     "image": "https://trvimg.r10s.jp/share/image_up/130594/origin/83120f7056614128296fbb5db1c6962d490e2f6f.47.9.26.3.jpg?fit=inside%7C888:498",
     "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F130594%2F"
-  },
-  {
-    "name": "オリオンホテル　モトブリゾート＆スパ",
-    "area": "恩納・名護",
-    "address": "沖縄県国頭郡本部町備瀬148-1",
-    "review": "4.54",
-    "review_count": 859,
-    "min_charge": 14030,
-    "image": "https://trvimg.r10s.jp/share/image_up/145419/origin/ad1c95df164174a38ae9107b1ac1554ed7bd0ea0.47.9.26.3.jpg?fit=inside%7C888:498",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F145419%2F"
   },
   {
     "name": "ＡＱＵＡＳＥＮＳＥ　Ｈｏｔｅｌ　＆　Ｒｅｓｏｒｔ",
