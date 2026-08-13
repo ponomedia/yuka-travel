01e44a9ad2f01389b674acfb2505c50db0d8fe8b
ponomedia <hayato0626hawaks@gmail.com>
2026-08-13 14:02:44 +0900

    add: 南の美ら花　ホテル　ミヤヒラ　＜石垣島＞ (石垣・西表)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "南の美ら花　ホテル　ミヤヒラ　＜石垣島＞",
+    "area": "石垣・西表",
+    "address": "沖縄県石垣市美崎町4-9",
+    "review": "4.34",
+    "review_count": 950,
+    "min_charge": 7296,
+    "image": "https://trvimg.r10s.jp/share/image_up/4653/origin/17ab9ab60ebda3e4041b944b280907743fb58e50.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F4653%2F"
+  },
+  {
     "name": "石垣島ビーチホテルサンシャイン＜石垣島＞",
     "area": "石垣・西表",
     "address": "沖縄県石垣市新川2484",
