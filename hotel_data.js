7215df88cccf6b9f9f0a58068ff449af84dfc61c
ponomedia <hayato0626hawaks@gmail.com>
2026-08-13 09:02:26 +0900

    add: 石垣島ビーチホテルサンシャイン＜石垣島＞ (石垣・西表)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "石垣島ビーチホテルサンシャイン＜石垣島＞",
+    "area": "石垣・西表",
+    "address": "沖縄県石垣市新川2484",
+    "review": "4.54",
+    "review_count": 369,
+    "min_charge": 6600,
+    "image": "https://trvimg.r10s.jp/share/image_up/13633/origin/0f7a072b0e155ebb4dff6713a37c0164647bd7b4.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F13633%2F"
+  },
+  {
     "name": "グランヴィリオリゾート石垣島　Ｏｃｅａｎ’ｓ　Ｗｉｎｇ　＆　Ｖｉｌｌａ　Ｇａｒｄｅｎ＜石垣島＞",
     "area": "石垣・西表",
     "address": "沖縄県石垣市新川舟蔵2481-1",
