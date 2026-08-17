f0716a6f3ea50cda9d5c9edb044b8c658fee430b
ponomedia <hayato0626hawaks@gmail.com>
2026-08-17 18:02:27 +0900

    add: 森の栖リゾート＆スパ (加賀温泉)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "森の栖リゾート＆スパ",
+    "area": "加賀温泉",
+    "address": "石川県加賀市山代温泉14-27",
+    "review": "4.6",
+    "review_count": 464,
+    "min_charge": 6600,
+    "image": "https://trvimg.r10s.jp/share/image_up/17927/origin/a8c22ba706bb2b44d12f505c865385f519ade178.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F17927%2F"
+  },
+  {
     "name": "亀の井ホテル　草津リゾート（旧：草津温泉ホテルリゾート）",
     "area": "草津温泉",
     "address": "群馬県吾妻郡草津町草津15-8",
