67d734748e4ebf81aff771bfe819de5a7d86f49b
ponomedia <hayato0626hawaks@gmail.com>
2026-08-18 18:02:34 +0900

    add: 城崎温泉　きのさきの宿　緑風閣 (城崎温泉)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "城崎温泉　きのさきの宿　緑風閣",
+    "area": "城崎温泉",
+    "address": "兵庫県豊岡市城崎町湯島174",
+    "review": "4.68",
+    "review_count": 735,
+    "min_charge": 25479,
+    "image": "https://trvimg.r10s.jp/share/image_up/176571/origin/47144449b6802fa5e6d7f45de2656d50dba0b5ed.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F176571%2F"
+  },
+  {
     "name": "城崎温泉　湯楽　Ｙｕｒａｋｕ　Ｋｉｎｏｓａｋｉ　Ｓｐａ＆Ｇａｒｄｅｎｓ",
     "area": "城崎温泉",
     "address": "兵庫県豊岡市城崎町湯島844",
