5c40bf89453f21e080937371b26e3208fc9898ea
ponomedia <hayato0626hawaks@gmail.com>
2026-08-28 11:01:59 +0900

    add: 季の湯　雪月花（共立リゾート） (箱根)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "季の湯　雪月花（共立リゾート）",
+    "area": "箱根",
+    "address": "神奈川県足柄下郡箱根町強羅1300-34",
+    "review": "4.24",
+    "review_count": 4026,
+    "min_charge": 18150,
+    "image": "https://trvimg.r10s.jp/share/image_up/54102/origin/3904b49023c8f6c79a6dbe09fb67dbb3d380b45b.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F54102%2F"
+  },
+  {
     "name": "富士山を一望できる宿　ホテルグリーンプラザ箱根",
     "area": "箱根",
     "address": "神奈川県足柄下郡箱根町仙石原1244-2",
@@ -1679,16 +1689,6 @@ const HOTEL_LIST = [
     "min_charge": 7986,
     "image": "https://trvimg.r10s.jp/share/image_up/1459/origin/4ee23425471eb4779c4f743b0fc5dc0fb7e266a1.47.9.26.3.jpg?fit=inside%7C888:498",
     "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F1459%2F"
-  },
-  {
-    "name": "季の湯　雪月花（共立リゾート）",
-    "area": "箱根",
-    "address": "神奈川県足柄下郡箱根町強羅1300-34",
-    "review": "4.22",
-    "review_count": 3969,
-    "min_charge": 21450,
-    "image": "https://trvimg.r10s.jp/share/image_up/54102/origin/3904b49023c8f6c79a6dbe09fb67dbb3d380b45b.47.9.26.3.jpg?fit=inside%7C888:498",
-    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F54102%2F"
   },
   {
     "name": "箱根湯本温泉　女性に優しい癒しの宿　ホテルマイユクール祥月",
