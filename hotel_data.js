a4606264fbbed186c3e7320041a8c15044fb59c0
ponomedia <hayato0626hawaks@gmail.com>
2026-09-09 14:02:13 +0900

    add: 箱根湯本温泉　ホテルおくゆもと (箱根)

--- hotel_data.js
+++ hotel_data.js
@@ -1,6 +1,16 @@
 // このファイルはtravel_hotel_poster.pyが自動更新します
 const HOTEL_LIST = [
   {
+    "name": "箱根湯本温泉　ホテルおくゆもと",
+    "area": "箱根",
+    "address": "神奈川県足柄下郡箱根町湯本茶屋211",
+    "review": "4.44",
+    "review_count": 2918,
+    "min_charge": 11000,
+    "image": "https://trvimg.r10s.jp/share/image_up/5081/origin/81ea89b2d4a411567263590bf780c914d1631b61.47.9.26.3.jpg?fit=inside%7C888:498",
+    "affiliate_url": "https://hb.afl.rakuten.co.jp/hgc/52ac58bc.05f4f7c6.52ac58bd.4392e6e1/?pc=https%3A%2F%2Ftravel.rakuten.co.jp%2FHOTEL%2F5081%2F"
+  },
+  {
     "name": "下呂温泉　離れの宿　月のあかり",
     "area": "下呂温泉",
     "address": "岐阜県下呂市湯之島758-15",
